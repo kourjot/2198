1369e9411fb7a0ef46544c6663212e2b2f24c4e7
kourjot <pkour6188@gmail.com>
2025-01-17 17:48:40 +0530

    signup

--- backend/router/userRouter.js
+++ backend/router/userRouter.js
@@ -1,7 +1,7 @@
 import {Router} from "express"
 import {createUser,userlogin,getUser} from "../controller/usercontroller.js";
 const userRouter=Router();
-userRouter.post("/create",createUser);
+userRouter.post("/signup",createUser);
 userRouter.post("/login",userlogin);
 userRouter.get("/get",getUser);
 export {userRouter}
