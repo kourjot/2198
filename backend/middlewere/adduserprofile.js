b86f94bf87837594d9423204c4a5d75f91a2494b
vijaynimar <vijaynimar8@gmail.com>
2025-01-19 01:25:53 +0530

    updateprofile

--- backend/middlewere/adduserprofile.js
+++ backend/middlewere/adduserprofile.js
@@ -1,10 +1,15 @@
+import jwt from "jsonwebtoken"
+import { User } from "../model/userModel.js";
 export const updateprofile=async (req, res) => {
     if (req.body.email) {
       return res.status(400).json({ message: "You cannot change email" });
     }
-  
-    const token = req.headers.authorization;
     const { governmentId, hobbies, shortBio } = req.body;
+    if (!governmentId || !hobbies || !shortBio){
+        return res.status(404).json({message:"all fields are required"})
+    }
+    const token = req.headers.authorization;
+   
   
     if (!token) {
       return res.status(401).json({ message: "Authorization token is required" });
@@ -16,16 +21,14 @@ export const updateprofile=async (req, res) => {
         return res.status(404).json({ message: "User not found" });
       }
   
-      if (userExists.hobbies || userExists.shortBio || userExists.governmentId) {
-        return res.status(200).json({ message: "Profile already updated"});
-      }
+      if (!userExists.hobbies && !userExists.shortBio && !userExists.governmentId) {
         userExists.governmentId = governmentId;
         userExists.hobbies = hobbies;
         userExists.shortBio = shortBio;
         await userExists.save();
-  
-        // Return updated profile
         return res.status(200).json({ profile: userExists });
+      }
+      return res.status(200).json({ message: "Profile already contains information" });
       }catch (err) {
       return res.status(500).json({ message: "Error updating profile", error: err.message });
     }
