a07cbbe15f7be6a4eadf36107dcb813938fe01bc
vijaynimar <vijaynimar8@gmail.com>
2025-01-19 11:37:27 +0530

    getevent

--- backend/controller/eventcontroller.js
+++ backend/controller/eventcontroller.js
@@ -8,7 +8,6 @@ const createEvent=async(req,res)=>{
             await event.deleteOne({ title, category, date, time });
             return res.status(200).json({ message: "Event already completed" });
         }
-        console.log(eventDateTime)
         const existingEvent = await event.findOne({ title, category, date, time });
         if (existingEvent) {
             return res.status(400).json({ error: "Event with the same details already exists" });
@@ -20,12 +19,21 @@ const createEvent=async(req,res)=>{
         res.status(400).json({error:err.message})
     }
 }
-const getEvents =async(req,res)=>{
-    try{
-        const events=await event.find()
-        res.status(200).json({events})
-    }catch(err){
-        res.status(500).json({error:err.message})
+const getEvents = async (req, res) => {
+    try {
+        // Query the database for events
+        const events = await event.find();
+
+        if (events.length === 0) {
+            return res.status(404).json({ message: "No events found" });
+        }
+
+        // Send events as response
+        res.status(200).json({ event:events });
+    } catch (err) {
+        // Handle any potential errors
+        res.status(500).json({ error: err.message });
     }
 }
+
 export {createEvent,getEvents}
